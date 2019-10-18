458a8160c74d77cf1c2bfb88b9a20a7b445bfb03
Ianette Grady <54339016+ianetteg81@users.noreply.github.com>
2019-10-18 15:21:23 -0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ClassyCooksWeb.Data;
+
+namespace ClassyCooksWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ClassyCooksWeb.Data.Cooks> Cooks { get; set; }
+        public DbSet<ClassyCooksWeb.Data.Departments> Departments { get; set; }
+        public DbSet<ClassyCooksWeb.Data.Resturants> Resturants { get; set; }
+    }
+}
---
+++ Data/Cooks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassyCooksWeb.Data
+{
+    public class Cooks
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CID { get; set; }
+
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+
+        public int RID { get; set; }
+        [ForeignKey("RID")]
+        public Resturants resturant { get; set; }
+
+        //foreign key
+        public int DID { get; set; }
+        [ForeignKey("DID")]
+        public Departments department { get; set; }
+    }
+}
